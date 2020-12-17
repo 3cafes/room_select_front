bf8ef4358f854dcc18f7418e88a83148ba17c5c5
cafe <unptitkawa@gmail.com>
2020-12-17 18:23:47 +0100

    base

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<WelcomeSF />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeSF from './components/WelcomeSF.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		WelcomeSF,
+	},
+};
 </script>
 
 <style>
+@font-face {
+	font-family: 'FabrikatBold';
+	src: url(./fonts/Fabrikat-Bold.woff) format('woff');
+}
+@font-face {
+	font-family: 'PoppinsLight';
+	src: url(./fonts/Poppins-Light.ttf) format('truetype');
+}
+@font-face {
+	font-family: 'PoppinsMedium';
+	src: url(./fonts/Poppins-Medium.ttf) format('truetype');
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Poppins-Light, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 </style>
---
+++ src/components/WelcomeSF.vue
@@ -0,0 +1,69 @@
+<template>
+	<transition name="fade">
+		<div v-show="show_bg" class="background">
+			<transition appear name="fade">
+				<div class="message-container" v-show="show_text">
+					<span class="hello">Hello, </span>
+					STATION F
+					<span class="emphase">!</span>
+				</div>
+			</transition>
+		</div>
+	</transition>
+</template>
+
+<script>
+export default {
+	name: 'WelcomeSF',
+	data() {
+		return {
+			show_bg: true,
+			show_text: true,
+		};
+	},
+	mounted() {
+		setTimeout(() => {
+			this.show_text = false;
+		}, 1000);
+		setTimeout(() => {
+			this.show_bg = false;
+		}, 1500);
+	},
+};
+</script>
+
+<style scoped lang="scss">
+.background {
+	position: fixed;
+	z-index: 1;
+	top: 0;
+	left: 0;
+	height: 100vh;
+	width: 100vw;
+	background-color: $light;
+	display: flex;
+	align-content: center;
+	justify-content: center;
+}
+.message-container {
+	@include title-1;
+	color: $dark;
+	margin: auto;
+}
+.hello {
+	font-family: $font-text;
+}
+.emphase {
+	color: $main;
+}
+
+// animations
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s ease-in-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+</style>
